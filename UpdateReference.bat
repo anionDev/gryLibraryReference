2e0f89e502b24c1e1c1d192dabc2690255ff2db2
Marius Göcke <marius.goecke@gmail.com>
2019-04-06 09:11:17 +0200

    updated update-script

--- UpdateReference.bat
+++ UpdateReference.bat
@@ -1,4 +1,6 @@
 cd ../gryLibrary/GRYLibrary
 call GenerateReference.bat
-robocopy _site ../gryLibraryReference/Site /mir
-pause
+robocopy _site ../../gryLibraryReference/Site /mir
+cd ../../gryLibraryReference
+git add -A
+git commit -m "Updated Reference"
