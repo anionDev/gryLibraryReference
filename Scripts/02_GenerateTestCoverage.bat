4d753f2af5925ad15a4ab82d3202d50f074d1cff
Marius Göcke <marius.goecke@gmail.com>
2019-04-12 23:22:16 +0200

    fixed opencoverscript

--- Scripts/02_GenerateTestCoverage.bat
+++ Scripts/02_GenerateTestCoverage.bat
@@ -1,1 +1,4 @@
-OpenCover.Console -target:"..\..\gryLibrary\GRYLibraryTest\bin\Release\GRYLibraryTest.dll" -filter:"+[GRYLibrary]* -[GRYLibraryTest]*" -output:..\OpenCoverTests.xml -register:user
+where vstest.console > temp.txt
+set /p testexe=<temp.txt
+del temp.txt
+OpenCover.Console -target:"%testexe%" -targetargs:..\..\gryLibrary\GRYLibraryTest\bin\Release\GRYLibraryTest.dll -filter:"+[GRYLibrary]* -[GRYLibraryTest]*" -output:..\OpenCoverTests.xml -register:user
