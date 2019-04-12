cda3f3dc835dac7b03c821cba3fa1236a2ad2be9
Marius Göcke <marius.goecke@gmail.com>
2019-04-12 23:31:40 +0200

    added testcoverage report

--- Scripts/01_UpdateReference.bat
+++ Scripts/01_UpdateReference.bat
@@ -2,7 +2,4 @@ cd ../../gryLibrary/Scripts
 call GenerateReferenceLocal.bat
 if %errorlevel% neq 0 exit /b %errorlevel%
 robocopy ../GRYLibrary/_site ../../gryLibraryReference/Site /mir
-cd ../../gryLibraryReference
-git add -A
-git commit -m "Updated Reference"
-cd Scripts
+cd ../../gryLibraryReference/Scripts
