d0dc56cd6ff383e3c655c86061d25491ca0f8024
Marius Göcke <marius.goecke@gmail.com>
2019-04-12 23:07:08 +0200

    Updated Reference

--- Scripts/01_UpdateReference.bat
+++ Scripts/01_UpdateReference.bat
@@ -1,5 +1,6 @@
 cd ../../gryLibrary/Scripts
 call GenerateReferenceLocal.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
 robocopy ../GRYLibrary/_site ../../gryLibraryReference/Site /mir
 cd ../../gryLibraryReference
 git add -A
