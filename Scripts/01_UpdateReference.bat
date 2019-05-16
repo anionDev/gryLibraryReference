0f9caee7bd3f8b338b2b008d80c0ad413f4ca08c
Marius Göcke <marius.goecke@gmail.com>
2019-05-16 20:57:07 +0200

    updated reference-generation

--- Scripts/01_UpdateReference.bat
+++ Scripts/01_UpdateReference.bat
@@ -1,5 +1,5 @@
-cd ../../gryLibrary/Scripts
+pushd ..\..\gryLibrary\Scripts
 call GenerateReferenceLocal.bat
 if %errorlevel% neq 0 exit /b %errorlevel%
 robocopy ../GRYLibrary/ReferenceGeneration/_site ../../gryLibraryReference/Reference /mir
-cd ../../gryLibraryReference/Scripts
+popd
