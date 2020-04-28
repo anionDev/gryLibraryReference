e497205704f24bcb9e1350a7d9747bdd93555ba8
Marius Göcke <marius.goecke@gmail.com>
2020-04-28 20:53:43 +0200

    removed obsolete files

--- Scripts/01_UpdateReference.bat
+++
@@ -1,5 +0,0 @@
-pushd ..\..\gryLibrary\Scripts
-call GenerateReferenceLocal.bat
-if %errorlevel% neq 0 exit /b %errorlevel%
-robocopy ../GRYLibrary/ReferenceGeneration/_site ../../gryLibraryReference/Reference /mir
-popd
--- Scripts/02_GenerateTestCoverage.bat
+++
@@ -1,7 +0,0 @@
-where vstest.console > temp.txt
-set /p testexe=<temp.txt
-del temp.txt
-OpenCover.Console -target:"%testexe%" -targetargs:..\..\gryLibrary\GRYLibraryTest\bin\Release\GRYLibraryTest.dll -filter:"+[GRYLibrary]* -[GRYLibraryTest]*" -output:..\OpenCoverTests.xml -register:user -returntargetcode
-set openCoverExitCode=%errorlevel%
-rmdir /s /q TestResults
-IF NOT %openCoverExitCode% EQU 0 EXIT /B 1
